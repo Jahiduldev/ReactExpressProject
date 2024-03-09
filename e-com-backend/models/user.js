31c78da596ecf07fa99a26de8ed21fa6fb283fb2
Jahidul Islam <jahiduldev@gmail.com>
2024-03-09 18:56:14 +0600

    Login and signup

--- e-com-backend/models/user.js
+++ e-com-backend/models/user.js
@@ -1,54 +1,51 @@
-const {Scheme, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
 
-
-const userSchema = Scheme({
+const userSchema = Schema({
     name: {
         type: String,
-        required : true,
+        required: true,
         minlength: 3,
-        maxlength: 100
+        maxlenth: 100,
     },
-
     email: {
         type: String,
-        required : true,
-        minlength: 3,
+        required: true,
+        minlength: 5,
         maxlength: 255,
-        unique:true,
+        unique: true,
     },
-
     password: {
         type: String,
-        required : true,
+        required: true,
         minlength: 5,
-        maxlength: 1024
+        maxlength: 1024,
     },
     role: {
         type: String,
-        enum : ['user','admin'],
-        default: user,
-    },
-},{timestamps: true});
+        enum: ['user', 'admin'],
+        default: 'user',
+    }
+}, { timestamps: true });
 
-userSchema.methods.generateJWT = function(){
+userSchema.methods.generateJWT = function () {
     const token = jwt.sign({
         _id: this._id,
         email: this.email,
         role: this.role,
         name: this.name
-    },process.env.JWT_SECRET_KEY,{expiresIn: "7d"});
+    }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
     return token;
 }
 
 const validateUser = user => {
-    const scheme = Joi.object({
-        name: Joi.string().min(3).max(100).required,
-        email: Joi.string().min(5).max(255).required,
-        password: Joi.string().min(5).max(255).required,
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(100).required(),
+        email: Joi.string().min(5).max(255).required(),
+        password: Joi.string().min(5).max(255).required(),
     });
-    return scheme.validate(user);
+    return schema.validate(user);
 }
 
 module.exports.User = model('User', userSchema);
