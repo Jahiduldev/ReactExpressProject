a21d190b41b8318cb74b6d14de8851d2f4a647de
Jahidul Islam <jahiduldev@gmail.com>
2024-03-09 19:48:25 +0600

    frontend folder added

--- e-com-backend/server.js
+++ e-com-backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URL_LOCAL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-   // useCreateIndex: true,
+   //useCreateIndex: true,
 })
     .then(() => console.log("Connected to MongoDB!"))
     .catch(err => console.error("MongoDB Connection Failed!"));
