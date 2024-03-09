67a52bf04d63d5235721f5b6e458c09dd63754ae
Jahidul Islam <jahiduldev@gmail.com>
2024-03-09 17:49:39 +0600

    user model created

--- e-com-backend/app.js
+++ e-com-backend/app.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 
+const userRouter = require('./routes/userRouter');
+
 app.use(express.json());
 app.use(cors());
 
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'));
 }
+
+app.use('/api/user', userRouter);
 
 module.exports = app;
