c68575f1a6fb02d13f1f25a333ba8bddee6304b3
Jahidul Islam <jahidulislam@Jahiduls-MacBook-Pro.local>
2024-03-09 16:33:56 +0600

    database connect and app run on port 3000

---
+++ e-com-backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv/config');
+const app = require('./app')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL_LOCAL, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+})
+.then(()=> console.log("Connected to MongoDb"))
+.catch(err => console.error("MongoDB Connection Failed"));
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log(`App running on port ${port}!`)
+})
