1e89cefb15f5a61e2b9c4d80e1b461ef9cf0f0ea
Jahidul Islam <jahiduldev@gmail.com>
2024-03-10 20:43:56 +0600

    Sign in and registration implemented both api and frontend

--- e-com-frontend/src/components/UserDetails.js
+++ e-com-frontend/src/components/UserDetails.js
@@ -3,11 +3,9 @@ import {useParams} from 'react-router-dom'
 
 const UserDetails = () => {
   const param = useParams();
-
   return (
     <div>
-      <h2>User Details</h2>
-      <p>User ID: {JSON.stringify(param)}</p>
+      <p>User Details: {JSON.stringify(param)}</p>
     </div>
   );
 };
